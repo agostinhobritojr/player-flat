3cdf0ed392b9a2ca5ac4652fd7f3ed98f5c0f727
agostinhobritojr <agostinhobritojr@gmail.com>
2014-11-11 08:51:35 -0300

    comentei o fft

---
+++ html/search/functions_7.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['setatribute',['setAtribute',['../class_media_info.html#ac1a807582e0830e8f7a6167257a8b69a',1,'MediaInfo']]],
+  ['spectrograph',['Spectrograph',['../class_spectrograph.html#ae4a10124a7de6cec48ccd09db0ab5fb6',1,'Spectrograph']]]
+];
---
+++ html/search/functions_9.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['_7econtrols',['~Controls',['../class_controls.html#a546f121f630c5c9db5094f6838d9fed2',1,'Controls']]],
+  ['_7emediainfo',['~MediaInfo',['../class_media_info.html#a28c974ae197fcb8bd2e083c7cb4cb99c',1,'MediaInfo']]]
+];
